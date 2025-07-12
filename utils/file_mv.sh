17c9a5b765f5fc4c5051574a5d03998124fdaa65
pang <pangyouzhen@live.com>
2025-07-12 15:22:41 +0800

    fixed

--- utils/file_mv.sh
+++ utils/file_mv.sh
@@ -2,6 +2,7 @@ python file_mv.py ../workday_data/202*
 git status
 read -p "Do you want to add and commit these changes? (yes/no): " confirmation
 
+cd ..
 if [[ "$confirmation" == "yes" ]]; then
     git add .
     git commit -m "fixed"
